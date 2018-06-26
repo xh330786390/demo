4daad3e2b1cdb2d679b6845b6a45ba0f97b4df3d
xh330786390 <330786390@qq.com>
2018-06-26 16:06:29 +0800

    123

--- src/main/java/com/xh/utils/CounterLimit.java
+++ src/main/java/com/xh/utils/CounterLimit.java
@@ -2,19 +2,13 @@ package com.xh.utils;
 
 /**
  * @ClassName: CounterLimit.java
- * @Description: java绫讳綔鐢ㄦ弿杩�
+ * @Description: java类作用描述
  * @Author: tengxh
- * @CreateDate: 2018/6/26 14:29
- * @UpdateUser: 鏇存柊鑰�
- * @UpdateDate: 2018/6/26 14:29
- * @UpdateRemark: 鏇存柊璇存槑
+ * @CreateDate: 2018/6/26 16:01
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2018/6/26 16:01
+ * @UpdateRemark: 更新说明
  * @Version: 1.0
  */
 public class CounterLimit {
-	
-	
-	
-	
-	
-	
 }
